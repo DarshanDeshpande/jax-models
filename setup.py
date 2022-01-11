243710dafc5f74b3c9cb9a47a9e3d79dc526188b
DarshanDeshpande <darshan1504@gmail.com>
2022-01-12 00:40:41 +0530

    Updated PyPi version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(current_path, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="jax_models",
-    version="0.0.1",
+    version="0.0.2",
     description="Unofficial JAX implementation of Deep Learning models",
     long_description=long_description,
     long_description_content_type="text/markdown",
